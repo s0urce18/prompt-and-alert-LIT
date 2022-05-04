e50f96e4bce1c3a604f9b87f0ba9803dad34cc14
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-05-04 18:29:10 +0300

    Update prompt-and-alert-vpackage.js

--- prompt-and-alert-vpackage.js
+++ prompt-and-alert-vpackage.js
@@ -14,14 +14,18 @@ function getChar() { // function for getting read character's code
     return -1; // return if char isn't read
 }
 
-function readLine(sep = "") { // function for reading line with separating by sep
-    let s = "", char, c, end = "\r\n";
-    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) != -1); // moving reading start to first not \r or \n  char
+function readWord(end = " \r\n") { // function for reading word to end
+    let s = "", char, c;
+    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) != -1); // moving reading start to first not \r or \n char
     if (c != -1) s = char; // first char
     while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) == -1) { // reading chars while end char isn't found
         s += char; // adding chars in one string while they aren't run out
     }
-    return sep != "" ? s.split(sep) : s; // if sep != empty string return s separated by sep else return s
+    return s;
+}
+
+function readLine(sep = "") { // function for reading line with possibility of separating with sep
+    return sep != "" ? readWord('\r\n').split(sep).filter(x => x != "") : readWord('\r\n'); // if sep != empty string return readWord('\r\n') separated by sep and else return readWord('\r\n')
 }
 
 function alert(msg = "", withoutN = false) { // function for writing messages, withoutN - will output be checked on having \n at the end
@@ -35,5 +39,5 @@ function prompt(msg = "") {
 }
 // ******************************************
 // vpackage
-// (c) 2021-09-24 alkhizha, s0urce18
+// (c) 2022-05-04 alkhizha, s0urce18
 // ******************************************
