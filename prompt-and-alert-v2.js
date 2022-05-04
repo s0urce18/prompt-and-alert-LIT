fceabc1d9da1237e2d7ea6ddba0394069d711f66
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-05-04 10:30:34 +0300

    Create prompt-and-alert-v2.js

---
+++ prompt-and-alert-v2.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const fd = (process.platform === 'win32') ? process.stdin.fd : fs.openSync('/dev/tty', 'rs');
+const StringDecoder = require('string_decoder').StringDecoder;
+const decoder = new StringDecoder('utf8');
+function alert(msg = "", withoutN = false) {
+    if (!withoutN && msg != '' && msg[msg.length - 1] != '\n') msg += '\n';
+    fs.writeSync(process.stdout.fd, msg.toString()); }
+function getChar() {
+    const buf = new Buffer.alloc(1, 0, 'utf8');
+    let bytesRead = fs.readSync(fd, buf, 0, 1);
+    if (bytesRead > 0) return decoder.write(buf).charCodeAt(0);
+    return -1; }
+function prompt(msg = "") {
+    alert(msg, true);
+    let s = "", char, c, end = "\r\n";
+    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) != -1);
+    if (c != -1) s = char;
+    while ((c = getChar()) != -1 && end.indexOf(char = String.fromCharCode(c)) == -1) s += char;
+    return s; }
