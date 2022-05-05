5e51e9fabd3432d0e7598400bd3bef9deb41a820
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-05-05 08:27:27 +0300

    Update prompt-and-alert-v2.js

--- prompt-and-alert-v2.js
+++ prompt-and-alert-v2.js
@@ -1,7 +1,7 @@
-// ******************************************
+// *****************************************
 // Увесь цей фрагмент тексту ставимо
 // на початок програмного js-файлу
-// ******************************************
+// *****************************************
 const fs = require('fs');
 const fd = (process.platform === 'win32') ? process.stdin.fd : fs.openSync('/dev/tty', 'rs');
 const StringDecoder = require('string_decoder').StringDecoder;
